17e148299371995d081e84255400d6262c08aafa
Mihyaeru <mihyaeru21@gmail.com>
2016-03-14 21:25:49 +0900

    read chapter 1

---
+++ 01/baby.hs
@@ -0,0 +1,16 @@
+
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100 then x else x * 2
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+-- 1.5
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+length' xs = sum [1 | _ <- xs ]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+
+-- 1.6
+triples = [ (a,b,c) | c <- [1..10], a <- [1..10], b <- [1..10] ]
+rightTriangles = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2 ]
+rightTriangles' = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a+b+c == 24 ]
