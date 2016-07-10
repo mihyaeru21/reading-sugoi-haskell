148fa3eb0d5a706ee8f8d1b2e98ea553afa9939e
Mihyaeru <mihyaeru21@gmail.com>
2016-07-11 00:24:32 +0900

    read chapter 14 (-14.5)

---
+++ 14/stack.hs
@@ -0,0 +1,10 @@
+import Control.Monad.Trans.State
+
+type Stack = [Int]
+
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
